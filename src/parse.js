9685ff5cee72ee448dc270a5f9ddc73350a3404e
Johan Venter <johan@zeroblink.com>
2018-11-22 17:58:56 -0800

    Update to Slate 0.39.3

--- src/parse.js
+++ src/parse.js
@@ -1,151 +1,132 @@
 // @flow
-import type { SlateModel } from './types';
-import type { Options } from './options';
-import Tag from './tag';
-import { printString } from './utils';
+import type { SlateModel } from "./types";
+import type { Options } from "./options";
+import Tag from "./tag";
+import { printString } from "./utils";
+import schema from "./schema";
 
 // All Tag parsers
 const PARSERS = {
-    value: (value, options) => [
-        Tag.create({
-            name: 'value',
-            attributes: getAttributes(value, options),
-            children: parse(value.document, options)
-        })
-    ],
+  value: (value, options) => [
+    Tag.create({
+      name: "value",
+      attributes: getAttributes(value, options),
+      children: parse(value.document, options)
+    })
+  ],
+  // COMPAT
+  state: (state, options) => PARSERS.value(state, options),
+  document: (document, options) => [
+    Tag.create({
+      name: "document",
+      attributes: getAttributes(document, options, false),
+      children: document.nodes.flatMap(node => parse(node, options)).toArray()
+    })
+  ],
+  block: (block, options) => [
+    Tag.create({
+      name: canPrintAsShorthand(block) ? block.type : block.object,
+      attributes: getAttributes(block, options, canPrintAsShorthand(block)),
+      children: schema.isVoid(block) ? [] : block.nodes.flatMap(node => parse(node, options)).toArray()
+    })
+  ],
+  inline: (inline, options) => [
+    Tag.create({
+      name: canPrintAsShorthand(inline) ? inline.type : inline.object,
+      attributes: getAttributes(inline, options, canPrintAsShorthand(inline)),
+      children: inline.nodes.flatMap(node => parse(node, options)).toArray()
+    })
+  ],
+  text: (text, options) => {
     // COMPAT
-    state: (state, options) => PARSERS.value(state, options),
-    document: (document, options) => [
+    const leaves = text.getLeaves ? text.getLeaves() : text.getRanges();
+    const leavesTags = leaves.flatMap(leaf => parse(leaf, options)).toArray();
+    if (options.preserveKeys) {
+      return [
         Tag.create({
-            name: 'document',
-            attributes: getAttributes(document, options, false),
-            children: document.nodes
-                .flatMap(node => parse(node, options))
-                .toArray()
+          name: "text",
+          attributes: { key: text.key },
+          children: leavesTags
         })
-    ],
-    block: (block, options) => [
+      ];
+    } else if (options.strict && text.text === "") {
+      return [
         Tag.create({
-            name: canPrintAsShorthand(block) ? block.type : block.object,
-            attributes: getAttributes(
-                block,
-                options,
-                canPrintAsShorthand(block)
-            ),
-            children: block.isVoid
-                ? []
-                : block.nodes.flatMap(node => parse(node, options)).toArray()
+          name: "text",
+          children: leavesTags
         })
-    ],
-    inline: (inline, options) => [
-        Tag.create({
-            name: canPrintAsShorthand(inline) ? inline.type : inline.object,
-            attributes: getAttributes(
-                inline,
-                options,
-                canPrintAsShorthand(inline)
-            ),
-            children: inline.isVoid
-                ? []
-                : inline.nodes.flatMap(node => parse(node, options)).toArray()
-        })
-    ],
-    text: (text, options) => {
-        // COMPAT
-        const leaves = text.getLeaves ? text.getLeaves() : text.getRanges();
-        const leavesTags = leaves
-            .flatMap(leaf => parse(leaf, options))
-            .toArray();
-        if (options.preserveKeys) {
-            return [
-                Tag.create({
-                    name: 'text',
-                    attributes: { key: text.key },
-                    children: leavesTags
-                })
-            ];
-        } else if (options.strict && text.text === '') {
-            return [
-                Tag.create({
-                    name: 'text',
-                    children: leavesTags
-                })
-            ];
-        }
+      ];
+    }
 
-        return leavesTags;
-    },
-    leaf: (leaf, options) =>
-        leaf.marks.reduce(
-            (acc, mark) => [
-                Tag.create({
-                    name: canPrintAsShorthand(mark) ? mark.type : mark.object,
-                    attributes: getAttributes(
-                        mark,
-                        options,
-                        canPrintAsShorthand(mark)
-                    ),
-                    children: acc
-                })
-            ],
-            [
-                {
-                    print: () => printString(leaf.text)
-                }
-            ]
-        ),
-    // COMPAT
-    range: (range, options) => PARSERS.leaf(range, options)
+    return leavesTags;
+  },
+  leaf: (leaf, options) =>
+    leaf.marks.reduce(
+      (acc, mark) => [
+        Tag.create({
+          name: canPrintAsShorthand(mark) ? mark.type : mark.object,
+          attributes: getAttributes(mark, options, canPrintAsShorthand(mark)),
+          children: acc
+        })
+      ],
+      [
+        {
+          print: () => printString(leaf.text)
+        }
+      ]
+    ),
+  // COMPAT
+  range: (range, options) => PARSERS.leaf(range, options)
 };
 
 /*
  * Returns attributes (with or without key)
  */
 function getAttributes(
-    model: SlateModel,
-    options: Options,
-    // True to spread the data as attributes.
-    // False to keep it under `data` and to make `type` explicit
-    asShorthand: boolean = true
+  model: SlateModel,
+  options: Options,
+  // True to spread the data as attributes.
+  // False to keep it under `data` and to make `type` explicit
+  asShorthand: boolean = true
 ): Object {
-    let result = {};
+  let result = {};
 
-    // type
-    if (!asShorthand && model.type) {
-        result.type = model.type;
-    }
+  // type
+  if (!asShorthand && model.type) {
+    result.type = model.type;
+  }
 
-    // key
-    if (options.preserveKeys && model.key) {
-        result.key = model.key;
-    }
+  // key
+  if (options.preserveKeys && model.key) {
+    result.key = model.key;
+  }
 
-    // data
-    if (!asShorthand && !model.data.isEmpty()) {
-        result.data = model.data.toJSON();
-    } else {
-        // Spread the data as individual attributes
-        result = { ...result, ...model.data.toJSON() };
-    }
+  // data
+  if (!asShorthand && !model.data.isEmpty()) {
+    result.data = model.data.toJSON();
+  } else {
+    // Spread the data as individual attributes
+    result = { ...result, ...model.data.toJSON() };
+  }
 
-    // isVoid
-    if (!asShorthand && model.isVoid) {
-        result.isVoid = true;
-    }
+  // isVoid
+  if (!asShorthand && schema.isVoid(model)) {
+    result.isVoid = true;
+  }
 
-    return result;
+  return result;
 }
 
 /*
  * Parse a Slate model to a Tag representation
  */
 function parse(model: SlateModel, options: Options): Tag[] {
-    const object = model.object || model.kind;
-    const parser = PARSERS[object];
-    if (!parser) {
-        throw new Error(`Unrecognized Slate model ${object}`);
-    }
-    return parser(model, options);
+  const object = model.object || model.kind;
+  const parser = PARSERS[object];
+  if (!parser) {
+    throw new Error(`Unrecognized Slate model ${object}`);
+  }
+  return parser(model, options);
 }
 
 /*
@@ -153,9 +134,9 @@ function parse(model: SlateModel, options: Options): Tag[] {
  * (data spread into attributes)
  */
 function canPrintAsShorthand(model: SlateModel): boolean {
-    const validAttributeKey = key => /^[a-zA-Z]/.test(key);
+  const validAttributeKey = key => /^[a-zA-Z]/.test(key);
 
-    return model.data.every((value, key) => validAttributeKey(key));
+  return model.data.every((value, key) => validAttributeKey(key));
 }
 
 export default parse;
---
+++ src/schema.js
@@ -0,0 +1,9 @@
+import { Schema } from "slate";
+
+export default Schema.create({
+  blocks: {
+    image: {
+      isVoid: true
+    }
+  }
+});
